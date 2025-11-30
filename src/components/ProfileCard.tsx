410c58a84c81dc5d318abb2e984cd29a5cd7209a
Grantyy <grantgilvillareal2002@gmail.com>
2025-11-30 23:48:49 +0800

    Day 4 – Props

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -7,9 +7,26 @@ export default function ProfileCard() {
         isStudent: true
     }
 
+    function Car(props : {brand: string}) {
+        return <h2>This is a {props.brand}!</h2>;
+
+    }
+
+    function Phone(props : {model: string}) {
+        return <h2>This is a {props.model} phone!</h2>;
+    }
+
     return (
         <div className="profile-card">
             <h1> Hi I'm {user.name}, I'm {user.age} years old, </h1>
+        
+            <Car brand="Toyota" />
+            <Car brand="Honda" />
+            <Car brand="Ford" />
+        
+            <Phone model="iPhone" />
+            <Phone model="Samsung" />
+            <Phone model="Google Pixel" />
         </div>
     )
 }
